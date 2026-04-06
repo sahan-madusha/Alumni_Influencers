50c7dfff8cb34550e8378d966e9bad379cf2b765
sahan madusha <sahanmadusha001@gmail.com>
2026-04-06 22:31:28 +0530

    feat: add dotenv and pg dependencies and implement environment configuration module

---
+++ src/Config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const PORT = process.env.PORT;
+export const APP_NAME = process.env.APP_NAME;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const NODE_ENV = process.env.NODE_ENV;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME;
+export const DB_PORT = process.env.DB_PORT;
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const EMAIL_USER = process.env.EMAIL_USER;
+export const EMAIL_PASS = process.env.EMAIL_PASS;
+export const EMAIL_FROM = process.env.EMAIL_FROM;
